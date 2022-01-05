a93ba376b96cabff712d60ed26d2388b156fc726
faridhash26 <faridhash26@gmail.com>
2022-01-05 23:50:59 +0330

    refactoring the order by time

--- ecommerce/orders/views.py
+++ ecommerce/orders/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import get_object_or_404, redirect, render
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.base import View
 from django.views.generic.list import ListView
-from django.utils.dateparse import parse_date
 import datetime
 from django.views.generic.edit import UpdateView
 from django.urls import reverse
@@ -22,7 +21,7 @@ class ListOfOrders(LoginRequiredMixin, View):
         to_date=str(datetime.datetime(int(parsed_fromdate[0]) ,int(parsed_fromdate[2]),int(parsed_fromdate[1])).date())   
 
         orderlist = Order.objects.filter(order_of_orderitem__product__shop__author=request.user.id).values(
-            'id','createdAt', 'order_of_orderitem__product__shop__name', 'status', 'customer__username').order_by('createdAt')
+            'id','createdAt', 'order_of_orderitem__product__shop__name', 'status', 'customer__username').order_by('-createdAt')
 
         return render(request, 'adminshop/pages/orders_list.html', {'order_list': orderlist,"from_date":from_date,"to_date":to_date })
 
@@ -38,7 +37,7 @@ class ListOfOrders(LoginRequiredMixin, View):
         to_date=datetime.datetime(int(parsed_todate[0]) ,int(parsed_todate[1]),int(parsed_todate[2])).date()   
 
         orderlist = Order.objects.filter(order_of_orderitem__product__shop__author=request.user.id,status=status ,createdAt__gte=from_date,createdAt__lte=to_date ).values(
-            'id', 'createdAt','order_of_orderitem__product__shop__name', 'status', 'customer__username').order_by('createdAt')
+            'id', 'createdAt','order_of_orderitem__product__shop__name', 'status', 'customer__username').order_by('-createdAt')
 
         return render(request, 'adminshop/pages/orders_list.html', {'order_list': orderlist ,"from_date":fromdate,"to_date":todate })
 
@@ -53,7 +52,7 @@ class ListOfOrderItems(LoginRequiredMixin, ListView):
         context = super().get_context_data(**kwargs)
         obj = get_object_or_404(Order, id=self.kwargs.get('oreder_id'))
 
-        context['order_item_list'] = OrderItem.objects.filter(order=obj).values(
+        context['order_item_list'] = OrderItem.objects.filter(order=obj ,product__shop__author__id = self.request.user.id).values(
             'product__name', 'qty', 'price', 'product__image')
         return context
 
@@ -61,7 +60,6 @@ class ListOfOrderItems(LoginRequiredMixin, ListView):
 class ChangeOrderStatus(LoginRequiredMixin, UpdateView):
     template_name = "adminshop/forms/change_shop_status.html"
     model = Order
-    # lookup_field='id'
     success_url =reverse_lazy("orders:the_orders")
     fields = [
         "status",
